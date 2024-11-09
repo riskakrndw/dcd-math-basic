92f04cd42174517f7214287916e29de59b877727
Riska Kurnia Dewi <riska@farmacare.id>
2024-11-09 23:13:09 +0800

    add calculation and unit test

--- src/MathBasic.js
+++ src/MathBasic.js
@@ -1,5 +1,5 @@
 const MathBasic = {
-  add: (...args) => {
+  _validateArgs: (args) => {
     if (args.length !== 2) {
       throw new Error("fungsi hanya menerima dua parameter");
     }
@@ -9,12 +9,24 @@ const MathBasic = {
     if (typeof a !== "number" || typeof b !== "number") {
       throw new Error("fungsi hanya menerima parameter number");
     }
-
+    return args;
+  },
+  add(...args) {
+    const [a, b] = this._validateArgs(args);
     return a + b;
   },
-  subtract: () => {},
-  multiply: () => {},
-  divide: () => {},
+  subtract(...args) {
+    const [a, b] = this._validateArgs(args);
+    return a - b;
+  },
+  multiply(...args) {
+    const [a, b] = this._validateArgs(args);
+    return a * b;
+  },
+  divide(...args) {
+    const [a, b] = this._validateArgs(args);
+    return a / b;
+  },
 };
 
 module.exports = MathBasic;
